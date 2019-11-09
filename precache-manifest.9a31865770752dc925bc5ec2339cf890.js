8feaeb1ef8ba2c87aeeb17e70b01bb2f7cc93a13
duncan <duncan.tv8@gmail.com>
2019-11-08 22:55:48 -0500

    Updates

---
+++ precache-manifest.9a31865770752dc925bc5ec2339cf890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a68371c9fb04381450d5a6d1139346c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3b47790b9d2cdaab00f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3af833f4f9d9fafd94a8",
+    "url": "/static/js/2.da356c9f.chunk.js"
+  },
+  {
+    "revision": "b3b47790b9d2cdaab00f",
+    "url": "/static/js/main.ca6859ed.chunk.js"
+  },
+  {
+    "revision": "a6e524d1510559be584b",
+    "url": "/static/js/runtime-main.2154f7ae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
